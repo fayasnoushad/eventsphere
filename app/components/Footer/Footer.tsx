9e02df42cb601d3f6521f4f39bf6478897ba8dee
Fayas <thefayasnoushad@gmail.com>
2025-12-18 17:53:32 +0530

    Update footer to include copyright information and license link

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="footer text-xs md:text-sm sm:footer-horizontal footer-center bg-base-300 text-base-content min-h-[10vh] p-2 md:p-4">
-      <aside>Copyright &copy; Coding Club (Sadanam Kumaran College)</aside>
+    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content min-h-[10vh] p-4">
+      <aside>
+        <Link
+          href="https://www.gnu.org/licenses/gpl-3.0.en.html"
+          target="_blank"
+        >
+          Copyright &copy; {new Date().getFullYear()} - Fest Management Website
+          <br />
+          GNU Affero General Public License v3 (GNU AGPL v3)
+        </Link>
+      </aside>
     </footer>
   );
 }
